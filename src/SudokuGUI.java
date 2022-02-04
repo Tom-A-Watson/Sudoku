0f498e4063a64110351530614b9b82a8a7107d72
Tom Watson <thomasw2701@gmail.com>
2022-02-04 01:37:09 +0000

    Added base JavaFX GUI and declared its size.
    Signed-off-by: Tom Watson <thomasw2701@gmail.com>

---
+++ src/SudokuGUI.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SudokuGUI extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.show();
+        stage.setWidth(500.0);
+        stage.setHeight(500.0);
+    }
+}
